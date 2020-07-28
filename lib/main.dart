a000bc813bb6d57e879a7512ff7c87933e02e754
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-07-28 19:10:09 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:Snek/snek.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snek',
+      home: Snek(),
+    );
+  }
+}
---
+++ lib/snek.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Snek extends StatefulWidget {
+  @override
+  _SnekState createState() => _SnekState();
+}
+
+class _SnekState extends State<Snek> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
