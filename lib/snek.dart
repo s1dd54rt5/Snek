06fd331a5a0454d91b63da7ddaaf07a4d82b86d2
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-07-28 19:42:25 +0530

    snake appears

--- lib/snek.dart
+++ lib/snek.dart
@@ -6,8 +6,50 @@ class Snek extends StatefulWidget {
 }
 
 class _SnekState extends State<Snek> {
+  static List snakePosition = [22, 42, 62];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              child: Container(
+                child: GridView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 700,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 20),
+                    itemBuilder: (BuildContext context, int index) {
+                      if (snakePosition.contains(index)) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Color(0xffef335e),
+                          ),
+                          margin: EdgeInsets.all(1),
+                          width: 5,
+                          height: 5,
+                        );
+                      } else {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Color(0xff121212),
+                          ),
+                          margin: EdgeInsets.all(1),
+                          width: 5,
+                          height: 5,
+                        );
+                      }
+                    }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
