fe3cbfe3e615e372cc04b0deb424eb93a5c18b51
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-07-28 22:24:22 +0530

    almost done

--- lib/snek.dart
+++ lib/snek.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Snek extends StatefulWidget {
@@ -7,61 +8,111 @@ class Snek extends StatefulWidget {
 }
 
 class _SnekState extends State<Snek> {
+  int score = 0;
+
   void startGame() {
+    score = 0;
+    snakePosition = [22, 42, 62];
     const duration = const Duration(milliseconds: 300);
     Timer.periodic(duration, (Timer timer) {
       moveSnake();
+      if (gameOver()) {
+        timer.cancel();
+        showGameOverScreen();
+      }
     });
+  }
+
+  bool gameOver() {
+    for (int i = 0; i < snakePosition.length; i++) {
+      int count = 0;
+      for (int j = 0; j < snakePosition.length; j++) {
+        if (snakePosition[i] == snakePosition[j]) {
+          count += 1;
+        }
+        if (count == 2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void showGameOverScreen() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Game Over'),
+            content:
+                Text('You\'re score: ' + (snakePosition.length - 3).toString()),
+            actions: [
+              FlatButton(
+                child: Text('Play Again'),
+                onPressed: () {
+                  startGame();
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   List snakePosition = [22, 42, 62];
 
-  var direction = 'right';
+  static var randomNumber = Random();
+  int food = randomNumber.nextInt(500);
+  void generateNewFood() {
+    food = randomNumber.nextInt(500);
+  }
+
+  var direction = 'left';
 
   void moveSnake() {
     setState(() {
       switch (direction) {
         case 'down':
-          if (snakePosition.last + 20 > 760) {
-            snakePosition.add(snakePosition.last + 20 - 760);
-            snakePosition.remove(snakePosition.first);
+          if (snakePosition.last + 20 > 500) {
+            snakePosition.add(snakePosition.last + 20 - 500);
           } else {
             snakePosition.add(snakePosition.last + 20);
-            snakePosition.remove(snakePosition.first);
           }
           break;
 
         case 'up':
           if (snakePosition.last - 20 < 0) {
-            snakePosition.add(snakePosition.last - 20 + 760);
-            snakePosition.remove(snakePosition.first);
+            snakePosition.add(snakePosition.last - 20 + 500);
           } else {
             snakePosition.add(snakePosition.last - 20);
-            snakePosition.remove(snakePosition.first);
           }
           break;
 
         case 'left':
           if (snakePosition.last % 20 == 0) {
             snakePosition.add(snakePosition.last - 1 + 20);
-            snakePosition.remove(snakePosition.first);
           } else {
             snakePosition.add(snakePosition.last - 1);
-            snakePosition.remove(snakePosition.first);
           }
           break;
 
         case 'right':
           if (snakePosition.last % 20 == 19) {
             snakePosition.add(snakePosition.last + 1 - 20);
-            snakePosition.remove(snakePosition.first);
           } else {
             snakePosition.add(snakePosition.last + 1);
-            snakePosition.remove(snakePosition.first);
           }
           break;
 
         default:
+      }
+      if (snakePosition.last == food) {
+        setState(() {
+          score = score + 1;
+        });
+        generateNewFood();
+      } else {
+        snakePosition.remove(snakePosition.first);
       }
     });
   }
@@ -72,52 +123,139 @@ class _SnekState extends State<Snek> {
       backgroundColor: Colors.black,
       body: Column(
         children: [
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            child: Text(
+              'Snek',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
           Expanded(
             child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (direction != 'up' && details.delta.dy > 0) {
+                  direction = 'down';
+                } else if (direction != 'down' && details.delta.dy < 0) {
+                  direction = 'up';
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (direction != 'left' && details.delta.dx > 0) {
+                  direction = 'right';
+                } else if (direction != 'right' && details.delta.dx < 0) {
+                  direction = 'left';
+                }
+              },
               child: Container(
                 child: GridView.builder(
                     physics: NeverScrollableScrollPhysics(),
-                    itemCount: 760,
+                    itemCount: 500,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 20),
                     itemBuilder: (BuildContext context, int index) {
                       if (snakePosition.contains(index)) {
                         return Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Color(0xffef335e),
+                          padding: EdgeInsets.all(1),
+                          child: ClipRRect(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xffef335e),
+                              ),
+                              width: 5,
+                              height: 5,
+                            ),
                           ),
-                          margin: EdgeInsets.all(1),
-                          width: 5,
-                          height: 5,
+                        );
+                      }
+                      if (index == food) {
+                        return Container(
+                          padding: EdgeInsets.all(1),
+                          child: ClipRRect(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xff5fef33),
+                              ),
+                              width: 5,
+                              height: 5,
+                            ),
+                          ),
                         );
                       } else {
                         return Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            color: Color(0xff121212),
+                          padding: EdgeInsets.all(1),
+                          child: ClipRRect(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xff121212),
+                              ),
+                              width: 5,
+                              height: 5,
+                            ),
                           ),
-                          margin: EdgeInsets.all(1),
-                          width: 5,
-                          height: 5,
                         );
                       }
                     }),
               ),
             ),
           ),
-          FlatButton(
-            onPressed: () {
-              startGame();
-            },
-            child: Container(
-              child: Text(
-                'Start',
-                style: TextStyle(
+          Container(
+            padding: EdgeInsets.only(left: 15),
+            alignment: Alignment.topLeft,
+            width: double.infinity,
+            child: Text(
+              score.toString(),
+              style: TextStyle(
                   color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+          Row(
+            children: [
+              FlatButton(
+                onPressed: () {
+                  startGame();
+                },
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 15),
+                  child: Text(
+                    'Start',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w800),
+                  ),
                 ),
               ),
-            ),
+              Expanded(
+                child: Container(
+                  width: 1,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  startGame();
+                },
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 15),
+                  child: Text(
+                    'Stop',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
